df7f9a24e28e9e4638a5ce2de808349569879dd7
ambarkr21 <108140248+ambarkr21@users.noreply.github.com>
2025-10-14 14:50:32 +0530

    Update Assignment 2.sql

--- Day 2/Assignment 2.sql
+++ Day 2/Assignment 2.sql
@@ -1,1 +1,33 @@
+CREATE TABLE Products (
+    product_id integer PRIMARY KEY ,
+    product_name VARCHAR2(20) NOT NULL,
+    price integer CHECK (price >0),
+    qty integer DEFAULT 0 CHECK (qty>=0)
+);
+
+CREATE TABLE Customers(
+    customer_id integer PRIMARY KEY,
+    customer_name VARCHAR2(20) NOT NULL,
+    email varchar2(50) NOT NULL,
+    phone varchar2(15) NOT NULL
+);
+
+CREATE TABLE Order(
+    order_id INTEGER PRIMARY KEY,
+    customer_id integer,
+    order_date DATE DEFAULT SYSDATE,
+    order_amt INTEGER CHECK (order_amt>0),
+    constraint fk_a FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE OrderDetails(
+    order_id INTEGER,
+    product_id INTEGER,
+    qty INTEGER,
+    constraint fk_b FOREIGN KEY (product_id) REFERENCES Products(product_id),
+    constraint fk_c FOREIGN KEY (order_id) REFERENCES Order(order_id),
+    constraint pk_a PRIMARY KEY(order_id,product_id)
+);
+
+
 
