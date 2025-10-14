f4b53adc7c1e93b8b8162a65fbbaf8b3c648e5b5
ambarkr21 <108140248+ambarkr21@users.noreply.github.com>
2025-10-14 15:25:31 +0530

    Update Assignment 2.sql

--- Day 2/Assignment 2.sql
+++ Day 2/Assignment 2.sql
@@ -1,3 +1,4 @@
+-- Create tables
 CREATE TABLE Products (
     product_id integer PRIMARY KEY ,
     product_name VARCHAR2(20) NOT NULL,
@@ -21,12 +22,12 @@ CREATE TABLE Orders(
 );
 
 CREATE TABLE OrderDetails(
-    order_details_id PRIMARY KEY,
+    order_details_id INTEGER PRIMARY KEY,
     order_id INTEGER,
     product_id INTEGER,
     qty INTEGER CHECK (qty>0),
     constraint fk_b FOREIGN KEY (product_id) REFERENCES Products(product_id),
-    constraint fk_c FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    constraint fk_c FOREIGN KEY (order_id) REFERENCES Orders(order_id)
 );
 
 -- Inserting products
@@ -37,16 +38,14 @@ INSERT INTO Products (product_id, product_name, price, qty) VALUES
 -- Inserting customers
 INSERT INTO Customers (customer_id, customer_name, email, phone) VALUES
 (1, 'John', 'john@example.com', '+919765544329'),
-(2,  'Smith', 'smith@gmail.com', '+919082345674');
+(2, 'Smith', 'smith@gmail.com', '+919082345674');
 
 -- Inserting orders
-INSERT INTO Orders (order_id, customer_id,order_date, order_amt) VALUES
-(1, 1,TO_DATE('2025-01-10','YYYY-MM-DD'), 5000),
-(2, 2, TO_DATE('2025-01-09','YYYY-MM-DD'),20000);
+INSERT INTO Orders (order_id, customer_id, order_date, order_amt) VALUES
+(1, 1, TO_DATE('2025-01-10','YYYY-MM-DD'), 5000),
+(2, 2, TO_DATE('2025-01-09','YYYY-MM-DD'), 20000);
 
 -- Inserting order details
-INSERT INTO OrderDetails (order_id, product_id, qty) VALUES
-(1,1,1,50),
-(2,2, 2, 100);
-
-
+INSERT INTO OrderDetails (order_details_id, order_id, product_id, qty) VALUES
+(1, 1, 1, 50),
+(2, 2, 2, 100);
