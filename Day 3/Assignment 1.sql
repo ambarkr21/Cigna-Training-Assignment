9e97f06df02947a46546f55e0f94b4ca2206e394
ambarkr21 <108140248+ambarkr21@users.noreply.github.com>
2025-10-15 14:14:19 +0530

    Update Assignment 1.sql

--- Day 3/Assignment 1.sql
+++ Day 3/Assignment 1.sql
@@ -1,1 +1,19 @@
+-- Create the Dept table
+CREATE TABLE Dept (
+    Deptno INT PRIMARY KEY,
+    Dname VARCHAR2(50),
+    Loc VARCHAR2(50)
+);
+
+-- Create the Emps table
+CREATE TABLE Emps (
+    Empno INT PRIMARY KEY,
+    Ename VARCHAR(50),
+    Job VARCHAR(50),
+    Sal DECIMAL(10, 2),
+    Deptno INT,
+    CONSTRAINT FK_A FOREIGN KEY (Deptno) REFERENCES Dept(Deptno)
+);
+
+
 
